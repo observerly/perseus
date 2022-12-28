b3187941d08bb9c93a50f70a518080a74bde17da
Michael J. Roberts <michael@observerly.com>
2022-12-28 12:53:50 +0000

    refactor: Amended to accept db Session type in seed functions.
    refactor: Amended to accept db Session type in seed functions.

--- app/init_db_seed.py
+++ app/init_db_seed.py
@@ -1,5 +1,7 @@
 import logging
 
+from sqlalchemy.orm import Session
+
 from app.db.session import SessionLocal
 from app.seed.messier import seed_messier
 from app.seed.stars import seed_stars
@@ -9,15 +11,17 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def seed() -> None:
-    db = SessionLocal()
+def seed(db: Session) -> None:
+    # Seed Messier Objects:
     seed_messier(db, logger)
+    # Seed Stars:
     seed_stars(db, logger)
 
 
 def main() -> None:
+    db = SessionLocal()
     logger.info("Populating Initial API Data Started")
-    seed()
+    seed(db)
     logger.info("Populating Initial API Data Success")
 
 
